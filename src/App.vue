7f8cae66630df7623c6981e06cd477efccc7f7b9
Ilya Averin <ilia.averin2@gmail.com>
2019-04-23 12:47:10 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,19 +12,12 @@
             :note="note" 
             @addNote="addNote"/>
           
-
           <!-- note list -->
-          <div class="notes">
-            <div class="note" v-for="(note, index) in notes" :key="index">
-              <div class="note-header">
-                <p>{{ note.title }}</p>
-              </div>
-              <div class="note-body">
-                  <p>{{ note.descr }}</p>
-                  <span>{{ note.date }}</span>
-              </div>
-            </div>
-          </div>
+					<notes 
+						:notes="notes"
+					/>
+					
+
         </div>
       </section>
 
@@ -35,10 +28,11 @@
 <script>
 import message from '@/components/Message.vue'
 import newNote from '@/components/NewNote.vue'
+import notes from '@/components/Notes.vue'
 
 export default {
   components: {
-    message, newNote
+    message, newNote, notes
   },
   data () {
     return {
