d572173e0ab03be85f7881d74aea6f835158be26
Ilya Averin <ilia.averin2@gmail.com>
2019-05-21 22:26:24 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,17 @@
           <newNote :note="note" @addNote="addNote"/>
           
 					<div class="note-header">
+						<!-- title -->
 						<h1>{{ title }}</h1>
+
+						<!-- search -->
+						<!-- <p>{{search}}</p> -->
+						<search 
+							:value = "search" 
+							placeholder = "Find note" 
+							@search = "search = $event"/>
+
+						<!-- icons control -->
 							<div class="icons">
 								<svg :class="{ active: grid }" @click="grid = true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
 								<svg :class="{ active: !grid }" @click="grid = false" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
@@ -18,7 +28,7 @@
 					</div>
 
           <!-- note list -->
-					<notes :notes="notes" :grid="grid" @remove="removeNote"/>
+					<notes :notes="notesFilter" :grid="grid" @remove="removeNote"/>
 					
 
         </div>
@@ -32,14 +42,17 @@
 import message from '@/components/Message.vue'
 import newNote from '@/components/NewNote.vue'
 import notes from '@/components/Notes.vue'
+import search from '@/components/Search.vue'
+import { filter } from 'minimatch';
 
 export default {
   components: {
-    message, newNote, notes
+    message, newNote, notes, search
   },
   data () {
     return {
-      title: 'Notes App',
+			title: 'Notes App',
+			search: '',
 			message: null,
 			grid: true,
 			note: {
@@ -64,7 +77,24 @@ export default {
 				}
 			]
     }
-  },
+	},
+	computed: {
+		notesFilter() {
+			let array = this.notes,
+				search = this.search
+			if (!search) return array	
+
+			search = search.trim().toLowerCase()
+			// filter
+			array = array.filter(function (item) {
+				if (item.title.toLowerCase().indexOf(search) !== -1) {
+					return item
+				}
+			})
+			// Error
+			return array
+		}
+	},
   methods: {
 			addNote () {
 				// console.log(this.note)
